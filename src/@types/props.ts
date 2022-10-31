4cecef704a2c12a50ad648f042f35acc85eae341
DAMILARE OBAFEMI <damimmanuel7@gmail.com>
2022-10-31 14:30:53 +0100

    first Push

---
+++ src/@types/props.ts
@@ -0,0 +1,17 @@
+type BaseProps = {
+    id?: string | number;
+    className?: string;
+  };
+  
+  export type CardDto = {
+    name: string;
+    description: string;
+  };
+  
+  export type CustomCardProps = BaseProps &
+    Partial<CardDto> & {
+      deleteHandler?: (data?: any) => void;
+      edit?: (data?: any) => void;
+      type?: 'add' | 'update'
+    };
+  
